692143e3568a27ccfff2fd9ba4b3d1cf9cc94b23
zacklsiegel <142410249+zacklsiegel@users.noreply.github.com>
2023-12-05 09:11:52 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+scene.setBackgroundColor(1)
+let list = [
+15,
+20,
+25,
+30
+]
+let snake = sprites.create(img`
+    . . . . . . . . . . . 6 6 6 6 6 
+    . . . . . . . . . 6 6 7 7 7 7 8 
+    . . . . . . 8 8 8 7 7 8 8 6 8 8 
+    . . e e e e c 6 6 8 8 . 8 7 8 . 
+    . e 2 5 4 2 e c 8 . . . 6 7 8 . 
+    e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+    e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+    e 2 e e 2 2 2 2 e e e e c 6 8 . 
+    c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+    . c 2 e e e 2 e 2 4 2 2 2 2 c . 
+    . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+    . . . e c c e c 2 2 2 2 2 2 2 e 
+    . . . . . . . c 2 e e 2 2 e 2 c 
+    . . . . . . . c e e e e e e 2 c 
+    . . . . . . . . c e 2 2 2 2 c . 
+    . . . . . . . . . c c c c c . . 
+    `, SpriteKind.Player)
+snake.sayText(list)
+for (let index = 0; index <= 3; index++) {
+    list[index] = list[index + 100]
+}
+for (let value of list) {
+    snake.sayText(value)
+    pause(500)
+}
